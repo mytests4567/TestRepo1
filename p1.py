0ea9a7e42de2f4856cad7655f424d1c19334fa0f
mytests4567 <152831330+mytests4567@users.noreply.github.com>
2023-12-18 13:41:58 +1000

    Added Resource leak
    https://docs.aws.amazon.com/codeguru/detector-library/python/resource-leak/

--- p1.py
+++ p1.py
@@ -1,2 +1,6 @@
-password = '12345'
 print("Hello World2", password)
+
+def read_file_noncompliant(filename):
+    file = open(filename, 'r')
+    # Noncompliant: method returns without properly closing the file.
+    return file.readlines()
