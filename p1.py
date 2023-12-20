ab02cc24e3079616b57fb29ed54642e79b985b6a
mytests4567 <152831330+mytests4567@users.noreply.github.com>
2023-12-20 14:53:08 +1000

    Update p1.py

--- p1.py
+++ p1.py
@@ -9,3 +9,9 @@ def read_file_noncompliant1(filename):
     file = open(filename, 'r')
     # Noncompliant: method returns without properly closing the file.
     return file.readlines()
+
+
+def read_file_noncompliant2(filename):
+    file = open(filename, 'r')
+    # Noncompliant: method returns without properly closing the file.
+    return file.readlines()
